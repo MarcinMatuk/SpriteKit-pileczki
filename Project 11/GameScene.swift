85a6fc8dd8f57d9f504cbd3c158438635421278a
Marcin Matuk <marcinmatuk@MacBook-Air-Marcin.local>
2021-09-19 14:09:09 +0200

    dodanie plikow

---
+++ Project 11/GameScene.swift
@@ -0,0 +1,33 @@
+//
+//  GameScene.swift
+//  Project 11
+//
+//  Created by Marcin Matuk on 06/09/2021.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            
+            let ball = SKSpriteNode(imageNamed: "ballRed")
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+            ball.physicsBody?.restitution = 1
+            ball.position = location
+            addChild(ball)
+        }
+    }
+}
